00251e21e9bd0a16c426450f4bc07a8e4a463e05
Sailendrarawal15 <rawalsailendra7777@gmail.com>
2025-07-06 00:27:20 +0545

    Update vaccum_utility.c

--- vaccum_utility.c
+++ vaccum_utility.c
@@ -8,7 +8,7 @@ int absVal(int a) { return a < 0 ? -a : a; }
 
 int main() {
     int x, y;
-    printf("Enter the Size of the Grid (x*y): ");
+    printf("Entr the Size of the Grid (x*y): ");
     scanf("%d%d", &x, &y);
     int grid[x][y];
     srand(time(NULL));
